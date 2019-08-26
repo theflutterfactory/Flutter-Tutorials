4346d366af6b8476a592424221172dfb360978cb
Julian Currie <julian.currie@gmail.com>
2019-08-26 22:09:57 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coding with Curry - Flutter
+
+This is the repository for the [Coding with Curry](https://www.youtube.com/channel/UCLBhrKXc9CQczo1vBA6w_mA??sub_confirmation=1) Youtube channel.
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coding with Curry',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Coding with Curry'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Coding with Curry'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
