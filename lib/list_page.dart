e248ea21b62489e5928c1e28a2a173997df506c8
Julian Currie <julian.currie@gmail.com>
2019-09-19 18:35:53 +0700

    added a listview and 2 buttons to list page

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -4,17 +4,37 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Provider Test'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text(
-            'Hello',
-            style: TextStyle(fontSize: 40),
-          ),
+        appBar: AppBar(
+          title: Text('Provider Test'),
         ),
-      ),
-    );
+        body: Container(
+          margin: EdgeInsets.all(24),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) => Text('Placeholder'),
+                  itemCount: 1,
+                ),
+              ),
+              SizedBox(height: 100),
+              RaisedButton(
+                child: Text(
+                  'Button 1',
+                  style: TextStyle(color: Colors.blue, fontSize: 16),
+                ),
+                onPressed: () => {},
+              ),
+              RaisedButton(
+                child: Text(
+                  'Button 2',
+                  style: TextStyle(color: Colors.purple, fontSize: 16),
+                ),
+                onPressed: () => {},
+              ),
+            ],
+          ),
+        ));
   }
 }
