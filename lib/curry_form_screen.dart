e50ba3f5b295e25683f0e202e55e826977793767
Julian Currie <julian.currie@gmail.com>
2019-10-06 15:50:43 +0700

    created the sliver widgets for the Flutter Slivers video

--- lib/curry_form_screen.dart
+++ lib/curry_form_screen.dart
@@ -1,3 +1,4 @@
+import 'package:CWCFlutter/curry_sliver_header.dart';
 import 'package:flutter/material.dart';
 
 class CurryFormScreen extends StatefulWidget {
@@ -28,6 +29,18 @@ class _CurryFormScreenState extends State<CurryFormScreen> {
     );
   }
 
+  Widget _buildListWidget(Color color, String text) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 48),
+        ),
+      ),
+    );
+  }
+
   void _submit() {
     if (!_formKey.currentState.validate()) {
       return;
@@ -41,23 +54,74 @@ class _CurryFormScreenState extends State<CurryFormScreen> {
     print(_nameList.toString());
 
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(24),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Add Name', style: TextStyle(fontSize: 22)),
-              _buildNameField(),
-              SizedBox(height: 16),
-              RaisedButton(
-                child: Text('Submit'),
-                onPressed: _submit,
-              ),
-            ],
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text('Coding with Curry'),
+            backgroundColor: Colors.deepPurple,
+            expandedHeight: 200,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                  'https://chefjet.com/wp-content/uploads/2017/06/Jet-Tila-Thai-Chicken-Curry.jpg',
+                  fit: BoxFit.cover),
+            ),
           ),
-        ),
+          CurrySliverHeader(Colors.purple, "Header 1"),
+          CurrySliverHeader(Colors.deepPurple, "Header 2"),
+          SliverFixedExtentList(
+            itemExtent: 150,
+            delegate: SliverChildListDelegate([
+              _buildListWidget(Colors.purple, "Curry"),
+              _buildListWidget(Colors.blue, "Rice"),
+              _buildListWidget(Colors.purple, "Pizza"),
+              _buildListWidget(Colors.blue, "Hamburger"),
+              _buildListWidget(Colors.purple, "Noodles"),
+              _buildListWidget(Colors.blue, "Eggs"),
+              _buildListWidget(Colors.purple, "Salad"),
+            ]),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                margin: EdgeInsets.all(24),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text('Add Name', style: TextStyle(fontSize: 22)),
+                      _buildNameField(),
+                      SizedBox(height: 16),
+                      RaisedButton(
+                        child: Text('Submit'),
+                        onPressed: _submit,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ]),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, mainAxisSpacing: 8, crossAxisSpacing: 8),
+            delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.deepPurple,
+                child: Center(
+                  child: Text(
+                    _nameList[index],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              );
+            },
+            childCount: _nameList.length
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/curry_sliver_header.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CurrySliverHeader extends StatelessWidget {
+  final Color backgroundColor;
+  final String headerTitle;
+
+  CurrySliverHeader(this.backgroundColor, this.headerTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      pinned: true,
+      floating: false,
+      delegate: Delegate(backgroundColor, headerTitle),
+    );
+  }
+}
+
+class Delegate extends SliverPersistentHeaderDelegate {
+  final Color backgroundColor;
+  final String headerTitle;
+
+  Delegate(this.backgroundColor, this.headerTitle);
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: backgroundColor,
+      child: Center(
+        child: Text(
+          headerTitle,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 36,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 150;
+
+  @override
+  double get minExtent => 60;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
