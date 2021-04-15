7b14494bc452452e3c727f2e9fd80e288a5a044c
Julian Currie <>
2021-04-15 14:26:54 +0700

    removed user_controller

--- lib/controlller/user_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:CWCFlutter/model/user.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final companyProvider = Provider<String>((_) => 'Cheetah Coding');
-
-class UserController extends StateNotifier<User> {
-  UserController(User state) : super(state);
-}
