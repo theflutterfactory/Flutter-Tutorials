ce05c389e551e926f36c1b8c550f5f959963dd88
Julian Currie <>
2021-07-29 03:43:27 +0700

    added finished code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
+import 'package:CWCFlutter/controller/file_controller.dart';
 import 'package:CWCFlutter/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MultiProvider(
+        providers: [ChangeNotifierProvider(create: (_) => FileController())],
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    context.read<FileController>().readText();
+    context.read<FileController>().readUser();
+    context.read<FileController>().readImage();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        appBarTheme: AppBarTheme(color: Color(0xFFe67e00)),
+        appBarTheme: AppBarTheme(color: Colors.orange[800]),
       ),
       home: Home(),
     );
